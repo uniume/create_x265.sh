06c6dd2b95e98485dcb838a2ebe60eb91efe2822
uniume <wing4future@gmail.com>
2018-04-09 09:50:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# create_x265_sh
---
+++ create_x265.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+FFMPEG_COMMAND="time ffmpeg -loglevel fatal"
+FFMPEG_PRESET="-c:v libx265 -preset slow -crf 24 -aspect 16:9 -c:a libopus -ac 2"
+
+case $# in
+  0 ) echo "Userge: $0 source.mkv [from-time [to-time]]" ;;
+  1 ) ${FFMPEG_COMMAND} -i $1 ${FFMPEG_PRESET} ${1%.mkv}_x265.mkv ;;
+  2 ) ${FFMPEG_COMMAND} -ss $2 -i $1 ${FFMPEG_PRESET} ${1%.mkv}_x265.mkv ;;
+  3 ) ${FFMPEG_COMMAND} -ss $2 -i $1 -to $3 ${FFMPEG_PRESET} ${1%.mkv}_x265.mkv ;;
+esac
+
